13f5721aab0179ecbe1494db2dc182e3514e3577
jasonwhk <jason19888@gmail.com>
2026-05-05 05:35:37 +0200

    Add Pocket Universe Lab mobile-first Three.js experience

---
+++ pocket-universe/modes.js
@@ -0,0 +1,1 @@
+export const MODES=[{id:'galaxy',name:'Galaxy'},{id:'gravity',name:'Gravity Well'},{id:'solar',name:'Solar Playground'},{id:'blackhole',name:'Black Hole'},{id:'flight',name:'Starfield Flight'}];
